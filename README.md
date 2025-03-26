603f60512ed84d877c8b748c9058f4493a6450cd
Ayachi2225 <1728353049@qq.com>
2025-03-26 12:44:55 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-# helloworld
+# jmcomicsget
 
-AstrBot 插件模板
+~~封号模拟器~~ AstrBot 插件
 
-A template plugin for AstrBot plugin feature
+A template plugin for AstrBot 
 
-# 支持
+本插件主要依赖于jmcomic和astrbot
+jmcomic包详细使用请查阅：https://github.com/hect0x7/JMComic-Crawler-Python
+本插件默认安装jmcomic中转pdf插件和获取浏览器cookies插件，若希望进一步实现其他功能请查阅上述jmcomic源项目
+安装：请将clone本插件到"AstrBot\data\plugins"中
+(后续更新也在这里)
+\```bash
+git clone https://github.com/Ayachi2225/astrbot_plugin_jmcomicsget.git
+\```
 
-[帮助文档](https://astrbot.app)
+默认下载文件保存到D:\jmcomic_files，如需修改请修改option.yml文件中base_url和plugin项pdf_dir中地址（修改后还要修改main.py中读取pdf文件位置）
+ps：目前本插件仅能借助napcat发送文件（lagrange不行），且仅在qq个人号windows napcat上成功运行了。
+
+后续更新：
+我猜现在估计只能下载单本本子，多章本子估计报错，后续可能改进
--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+import jmcomic
+import astrbot.api.message_components as Comp
+import os,re
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("jmcomicsget", "ayachi", "下载禁漫本子", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.command("jm")
+    async def get_file(self,event):
+        chain = [
+            Comp.At(qq=event.get_sender_id()),  # At 消息发送者
+            Comp.Plain("你要的本子在来的路上了嗷（大约还需要等待30s）"),
+        ]
+        yield event.chain_result(chain)
+        message = event.message_str
+        s=''.join(re.findall(r'\d+', message))
+        current_file = os.path.abspath(__file__)
+        current_dir = os.path.dirname(current_file)
+        target_file = os.path.join(current_dir, 'option.yml')
+        option = jmcomic.JmOption.from_file(target_file)
+        jmcomic.download_album(s, option)
+        file = [Comp.File(file=f"D:\\jmcomic_files\\{s}.pdf",name=f"{s}.pdf")]  # 从本地文件目录发送pdf
+        # file = [Comp.Image.fromFileSystem(f"D:\\jmcomic_files\\{event.message_str[3:]}.pdf")]  # 从本地文件目录发送pdf
+        yield event.chain_result(file)
+    # async def uploda_file(self, event: AstrMessageEvent):
+    #       chain = [
+    #           Comp.At(qq=event.get_sender_id()), # At 消息发送者
+    #           Comp.Plain("你要的本子来了喵"),
+    #       ]
+    #       file = Comp.Image.fromFileSystem(f"D:\jmcomic_files\{event.message_str}.pdf") # 从本地文件目录发送pdf
+    #       yield event.chain_result(chain)
+    #       yield event.chain_result(file)
 
-    async def terminate(self):
-        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
+
+
+
